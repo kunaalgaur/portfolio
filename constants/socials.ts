3195a2136b65570c5355944b4c450e979fe5b947
kunaalgaur <kunaalgaurr@gmail.com>
2023-10-05 11:27:29 +0530

    Updated constants

--- constants/socials.ts
+++ constants/socials.ts
@@ -1,22 +1,24 @@
-export const socials = [
-  {
-    name: 'LinkedIn',
-    link: '',
-  },
-  {
-    name: 'Github',
-    link: '',
-  },
-  {
-    name: 'Instagram',
-    link: '',
-  },
-  {
-    name: 'Facebook',
-    link: '',
-  },
-  {
-    name: 'Twitter',
-    link: '',
-  },
+import { Social } from './types';
+
+export const socials: Social[] = [
+    {
+        name: 'LinkedIn',
+        link: '',
+    },
+    {
+        name: 'Github',
+        link: '',
+    },
+    {
+        name: 'Instagram',
+        link: '',
+    },
+    {
+        name: 'Facebook',
+        link: '',
+    },
+    {
+        name: 'Twitter',
+        link: '',
+    },
 ];
