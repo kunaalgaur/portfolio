8d12f4ec5de357efe5f1200bdb019b6c1c01c692
kunaalgaur <kunaalgaurr@gmail.com>
2023-10-05 11:33:27 +0530

    New components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
-// import Home from '@/components/card/Home/Home';
+import Home from '@/components/card/Home/Home';
+import Services from '@/components/card/Services/Services';
+import Work from '@/components/card/Work/Work';
+import React from 'react';
 
 const page = () => {
-  return <div>{/* <Home /> */}</div>;
+    return (
+        <div>
+            <Home />
+            <Services />
+            <Work />
+        </div>
+    );
 };
 
 export default page;
