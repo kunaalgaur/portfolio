8dc76d0ad9615bcd55416e3308b7dd98fbbc0500
kunaalgaur <kunaalgaurr@gmail.com>
2023-10-14 16:30:22 +0530

    Added constant utilities to faq's

---
+++ constants/questions.ts
@@ -0,0 +1,35 @@
+type Questions = {
+    question: string;
+    answer: string;
+};
+
+export const questions: Questions[] = [
+    {
+        question: 'Where are you from?',
+        answer: '🏡 Originally, I hail from the picturesque city of Dehradun, nestled in the heart of Uttarakhand, India. 🌄 However, I was born and raised in the bustling metropolis of Delhi. 🌆 While I have been calling Dehradun home for the last five years, Delhi still claims most of my time and heart. 🕰️🏙️',
+    },
+    {
+        question: 'What is your hourly rate?',
+        answer: `📊 Estimating project costs can be quite subjective, as it heavily depends on the project's complexity. However, for a rough estimate, you might expect to budget anywhere from $5 to $50 per hour. 💰💡`,
+    },
+    {
+        question: 'What is your phone number?',
+        answer: `📧 I have a strong preference for email communication in all my professional endeavors. You can reach me at kunal_gaur@outlook.com. However, if you ever need to connect with me via phone, feel free to call at +91 7303421656. I'm always prompt to answer your call. 📞📩`,
+    },
+    {
+        question: 'Are you married?',
+        answer: `🎉 I'm just 22 years young, and marriage isn't on my bucket list right now. So, to answer your question, no, I'm not married! 💍🙅‍♂️`,
+    },
+    {
+        question: 'How old are you?',
+        answer: `🎉 I'm a spry 22-year-old, and guess what? My birthday falls on the 17th of January! 🎂 Feel free to surprise me with a present if you're up for it! 🎁😄`,
+    },
+    {
+        question: 'What is your philosophy about design?',
+        answer: `🎨 Design plays a pivotal role in your project. I firmly believe that if you have to explain how to use it, it might not be the epitome of good design. 🖌️📐💡`,
+    },
+    {
+        question: 'What is your philosophy about server performance?',
+        answer: `⏳ Waiting for content to load for over 5 seconds? It's something I truly dislike. Let's make sure our server performance is optimized to the max! 🚀🔧`,
+    },
+];
