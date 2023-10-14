4aa7b5a73d5020780f35d2046e69f83fc1a7de78
kunaalgaur <kunaalgaurr@gmail.com>
2023-10-14 16:29:40 +0530

    Added social links

---
+++ constants/social.ts
@@ -0,0 +1,28 @@
+type Social = {
+    name: string;
+    image: string;
+    link: string;
+};
+
+export const socials: Social[] = [
+    {
+        name: 'LinkedIn',
+        image: '/linkedin.png',
+        link: 'https://www.linkedin.com/in/kunal-gaur-714621200/',
+    },
+    {
+        name: 'Github',
+        image: '/github.png',
+        link: 'https://github.com/kunaalgaur',
+    },
+    {
+        name: 'Instagram',
+        image: '/instagram.png',
+        link: 'https://www.instagram.com/kunall_gaurr/?hl=en',
+    },
+    {
+        name: 'Facebook',
+        image: '/facebook.png',
+        link: 'https://www.facebook.com/kunal.gaur.509994',
+    },
+];
