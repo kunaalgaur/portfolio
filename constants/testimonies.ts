c4d0729f490217d8e3f89c0defb51cc030d64cf8
kunaalgaur <kunaalgaurr@gmail.com>
2023-10-15 09:59:44 +0530

    Added new constants

---
+++ constants/testimonies.ts
@@ -0,0 +1,26 @@
+type Testimony = {
+    name: string;
+    position: string;
+    message: string;
+};
+
+export const testimonies: Testimony[] = [
+    {
+        name: 'Monika Milwani',
+        position: 'Head HR at NFTically',
+        message:
+            'It is my pleasure to recommend Mr. Kunal Gaur, who worked with us as an Intern - Node.js and React.js developer at NFTically from 13th June 2023 to 12th August 2023. Kunal has good communication skills, In addition to this, he is organized and reliable in his work. Kunal can work independently and can follow through to ensure the job get done. He is flexible and willing to work on projectsassigned to him. Kunal was quick to volunteer and assist in other areas of the company operations. Kunal would be a grat asset to any company and has my recommendation.',
+    },
+    {
+        name: 'Vikash Chandra',
+        position: 'Full stack developer at NFTically.',
+        message:
+            'Kunal is a highly proficient fullstack developer. I worked with him at NFTically. His technical prowess and ability to deliver high-quality code make him an invaluable asset. Kunal consistently exceeded expectations and his deep expertise in React & Node, helped us to deliver sprint on time. He is a top choice for fullstack development. I wholeheartedly recommend Kunal for any development work.',
+    },
+    {
+        name: 'Geetu Singh',
+        position: 'Head HR at Grolier International',
+        message:
+            'We are please to state that during his tenure Kunal was found to be sincere and hardworking and we widh thim best of luck for his professional career.',
+    },
+];
---
+++ constants/works.ts
@@ -0,0 +1,28 @@
+type Work = {
+    name: string;
+    image: string;
+    tech: string;
+};
+
+export const works = [
+    {
+        name: 'Threads - A social media pleatform',
+        image: '',
+        tech: 'UI/UX design, Front-end development, Back-end Developement',
+    },
+    {
+        name: 'Shopcart - An e-commerce platform',
+        image: '',
+        tech: 'UI/UX design, Front-end development, Back-end Developement',
+    },
+    {
+        name: 'Weather App',
+        image: '',
+        tech: 'UI/UX Design, Front-end development',
+    },
+    {
+        name: 'Wealth360',
+        image: '',
+        tech: 'UI/UX Design, Front-end development',
+    },
+];
