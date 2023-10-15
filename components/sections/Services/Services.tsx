07686a933608ccfeb20a26db1b1deab325b18762
kunaalgaur <kunaalgaurr@gmail.com>
2023-10-15 10:02:08 +0530

    Added new sections

--- components/sections/Services/Services.tsx
+++ components/sections/Services/Services.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import styles from './Services.module.css';
+import ServicesCard from '@/components/card/ServicesCard/ServicesCard';
+import { BsTerminal, BsCodeSquare, BsBox } from 'react-icons/bs';
 
 const Services = () => {
-    return <div>Services</div>;
+    return (
+        <div id={styles.container}>
+            <div id={styles.background}>
+                <div className={styles.children}></div>
+                <div className={styles.children}></div>
+                <div className={styles.children}></div>
+                <div className={styles.children}></div>
+            </div>
+            <div id={styles.wrapper}>
+                <ServicesCard
+                    icon={<BsBox className={styles.icon} />}
+                    name="UI/UX Design"
+                    projects="4 projects"
+                />
+                <ServicesCard
+                    icon={<BsCodeSquare className={styles.icon} />}
+                    name="Front-end Development"
+                    projects="7 projects"
+                />
+                <ServicesCard
+                    icon={<BsTerminal className={styles.icon} />}
+                    name="Back-end Development"
+                    projects="4 projects"
+                />
+            </div>
+        </div>
+    );
 };
 
 export default Services;
