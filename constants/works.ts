659c41f5159a222c97cb2a1a8575e5f8cb46e091
kunaalgaur <kunaalgaurr@gmail.com>
2023-10-24 23:30:06 +0530

    Updated constants

--- constants/works.ts
+++ constants/works.ts
@@ -9,20 +9,24 @@ export const works = [
         name: 'Threads - A social media pleatform',
         image: '',
         tech: 'UI/UX design, Front-end development, Back-end Developement',
+        link: '',
     },
     {
         name: 'Shopcart - An e-commerce platform',
         image: '',
         tech: 'UI/UX design, Front-end development, Back-end Developement',
+        link: '',
     },
     {
         name: 'Weather App',
         image: '',
         tech: 'UI/UX Design, Front-end development',
+        link: '',
     },
     {
         name: 'Wealth360',
         image: '',
         tech: 'UI/UX Design, Front-end development',
+        link: 'https://www.thewealth360.com',
     },
 ];
